8c1241256fe30650e33a074d63015147abf51e9e
Guy Bedford <guybedford@gmail.com>
2021-05-11 22:12:40 +0200

    error recovery, better batching

---
+++ test/api/parallel.test.js
@@ -0,0 +1,14 @@
+import { Generator } from '#dev';
+import assert from 'assert';
+
+const generator = new Generator({
+  mapUrl: import.meta.url,
+  defaultProvider: 'jspm',
+  env: ['production', 'browser']
+});
+
+await Promise.all([generator.install('react@16'), generator.install('lit-element@2.5.1')]);
+const json = generator.getMap();
+console.log(json);
+assert.strictEqual(json.imports.react, 'https://ga.jspm.io/npm:react@16.14.0/index.js');
+assert.strictEqual(json.imports['lit-element'], 'https://ga.jspm.io/npm:lit-element@2.5.1/lit-element.js');
