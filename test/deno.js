7b51124e27f54c89f8ec05659b930ca127f85551
Guy Bedford <guybedford@gmail.com>
2021-07-01 08:59:17 -0700

    wip: deno support

--- test/deno.js
+++ test/deno.js
@@ -1,11 +1,24 @@
 import { Buffer } from 'buffer';
 import { execSync } from 'child_process';
+import { tmpdir } from 'os';
+import { resolve } from 'path';
+import { writeFileSync, unlinkSync } from 'fs';
 
-function dataUrl (contentType, source) {
-  return `data:${contentType};base64,${Buffer.from(source).toString('base64')}`;
-}
+// function dataUrl (contentType, source) {
+//   return `data:${contentType};base64,${Buffer.from(source).toString('base64')}`;
+// }
 
 export function denoExec (map, source) {
-  console.log(map);
-  execSync(`deno run --unstable --no-check --allow-all --import-map="${dataUrl('application/json+importmap', JSON.stringify(map))}" "${dataUrl('application/javascript', source)}"`);
+  const tmpDir = tmpdir();
+  const tmpMap = resolve(tmpDir, 'map.json');
+  const tmpSrc = resolve(tmpDir, 'app.js');
+  writeFileSync(tmpMap, JSON.stringify(map));
+  writeFileSync(tmpSrc, source);
+  try {
+    execSync(`deno run --unstable --no-check --allow-all --import-map=${tmpMap} ${tmpSrc}`);
+  }
+  finally {
+    unlinkSync(tmpMap);
+    unlinkSync(tmpSrc);
+  }
 }
