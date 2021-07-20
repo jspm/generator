6b9d2ac7ec670170487d7cfefa552bc596658062
Guy Bedford <guybedford@gmail.com>
2021-07-20 10:53:13 -0700

    more fixes

--- test/deno.js
+++ test/deno.js
@@ -17,9 +17,9 @@ export function denoExec (map, source) {
   writeFileSync(tmpSrc, source);
   try {
     execSync(`${process.env.DENO_BIN || 'deno'} run --unstable --no-check --allow-all --import-map=${tmpMap} ${tmpSrc}`);
-  }
-  finally {
     unlinkSync(tmpMap);
     unlinkSync(tmpSrc);
+  }
+  finally {
   }
 }
