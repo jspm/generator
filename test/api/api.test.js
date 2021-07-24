3895d5b131cd8ac4b7a8a1f9e43d2d371befaab9
Guy Bedford <guybedford@gmail.com>
2021-07-23 17:06:57 -0700

    add another analysis test

--- test/api/api.test.js
+++ test/api/api.test.js
@@ -12,3 +12,11 @@ const json = generator.getMap();
 assert.strictEqual(json.imports.react, 'https://ga.jspm.io/npm:react@16.14.0/index.js');
 
 assert.strictEqual(generator.importMap.resolve('react'), 'https://ga.jspm.io/npm:react@16.14.0/index.js');
+
+const meta = generator.getAnalysis('https://ga.jspm.io/npm:react@16.14.0/index.js');
+assert.deepStrictEqual(meta, {
+  format: 'esm',
+  staticDeps: ['./cjs/react.production.min.js', 'object-assign'],
+  dynamicDeps: [],
+  cjsLazyDeps: null
+});
