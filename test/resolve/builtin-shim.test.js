e53ade46bfbc4b2f0ec02be27a1165a4e178793a
Guy Bedford <guybedford@gmail.com>
2021-07-28 11:29:44 -0700

    fixup: add back test

--- test/resolve/builtin-shim.test.js
+++ test/resolve/builtin-shim.test.js
@@ -1,7 +1,7 @@
 import { Generator } from '@jspm/generator';
 import assert from 'assert';
 
-if (typeof document === 'undefined' && false) {
+if (typeof document === 'undefined') {
   const generator = new Generator({
     stdlib: new URL('../../jspm-core/', import.meta.url),
     mapUrl: import.meta.url,
@@ -13,23 +13,24 @@ if (typeof document === 'undefined' && false) {
 
   const json = generator.getMap();
 
-  console.log(json);
-
   assert.deepStrictEqual(json, {
-    imports: {
-      '../../node_modules/chalk/source/templates': '../../node_modules/chalk/source/templates.js',
-      '../../node_modules/chalk/source/util': '../../node_modules/chalk/source/util.js',
-      '../../node_modules/color-convert/conversions': '../../node_modules/color-convert/conversions.js',        
-      '../../node_modules/color-convert/route': '../../node_modules/color-convert/route.js',
-      chalk: '../../node_modules/chalk/source/index.js'
-    },
     scopes: {
-      '../../node_modules/ansi-styles/': { 'color-convert': '../../node_modules/color-convert/index.js' },      
-      '../../node_modules/chalk/': {
-        'ansi-styles': '../../node_modules/ansi-styles/index.js',
-        'supports-color': '../../node_modules/supports-color/browser.js'
+      '../../jspm-core/node_modules/buffer/': {
+        buffer: '../../jspm-core/nodelibs/buffer.js',
+        process: '../../jspm-core/nodelibs/process.js'
       },
-      '../../node_modules/color-convert/': { 'color-name': '../../node_modules/color-name/index.js' }
+      '../../jspm-core/node_modules/process/': {
+        buffer: '../../jspm-core/nodelibs/buffer.js',
+        process: '../../jspm-core/nodelibs/process.js'
+      },
+      './cjspkg/': {
+        'process/': './cjspkg/node_modules/process/index.js',
+        buffer: '../../jspm-core/nodelibs/buffer.js'
+      },
+      './cjspkg/node_modules/process/': {
+        buffer: '../../jspm-core/nodelibs/buffer.js',
+        process: '../../jspm-core/nodelibs/process.js'
+      }
     }
   });
 }
