b007194eec6869c1013bfb9c23e3a5eeec2e4f47
Guy Bedford <guybedford@gmail.com>
2021-08-30 20:54:51 -0700

    refine self generation

--- self-generate.js
+++ self-generate.js
@@ -1,18 +1,19 @@
 import { Generator } from '@jspm/generator';
-import assert from 'assert';
+
+const noTs = !process.env.TS;
 
 const generator = new Generator({
   mapUrl: 'about:blank',
-  inputMap: {
+  inputMap: noTs ? {
     imports: {
       '@babel/core': 'https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.8/nodelibs/@empty.js',
       '@babel/preset-typescript': 'https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.8/nodelibs/@empty.js'
     }
-  }
+  } : {}
 });
 
 await generator.install('@jspm/generator');
 
 
 const json = generator.getMap();
-console.log(json);
+console.log(JSON.stringify(json, null, 2));
