1437ad2efdaf7c2ed866bf76a967f629d25ed077
Guy Bedford <guybedford@gmail.com>
2021-10-19 14:32:21 -0700

    feat: expose source condition (#72)

--- test/deno/babel.test.js
+++ test/deno/babel.test.js
@@ -2,7 +2,8 @@ import { Generator } from '@jspm/generator';
 import { denoExec } from '#test/deno';
 
 const generator = new Generator({
-  env: ['node', 'deno']
+  env: ['node', 'deno'],
+  stdlib: '@jspm/core@2.0.0-beta.10' // Pending https://github.com/babel/babel/issues/13863
 });
 
 await generator.install('@babel/core@7.15.0');
