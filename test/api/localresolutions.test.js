c0266cc87504d9acd9b3ce2c1357032177dfadc4
Guy Bedford <guybedford@gmail.com>
2021-11-08 03:13:30 -0800

    import-map update

--- test/api/localresolutions.test.js
+++ test/api/localresolutions.test.js
@@ -13,5 +13,4 @@ const generator = new Generator({
 
 await generator.install({ target: './api/local/pkg', subpath: './withdep' });
 const json = generator.getMap();
-
-assert.strictEqual(json.scopes['./'].dep, './local/dep/main.js');
+assert.strictEqual(json.scopes['../'].dep, './local/dep/main.js');
