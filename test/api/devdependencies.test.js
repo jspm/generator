b77f0626774b805455d32133474d2d77a11dbfed
Guy Bedford <guybedford@gmail.com>
2021-11-25 13:07:10 -0800

    feat: devdependencies local scoping

---
+++ test/api/devdependencies.test.js
@@ -0,0 +1,12 @@
+import { Generator } from '@jspm/generator';
+import assert from 'assert';
+
+const generator = new Generator({
+  mapUrl: new URL('./local/pkg/asdf', import.meta.url),
+  defaultProvider: 'jspm',
+  env: ['production', 'browser']
+});
+
+await generator.traceInstall('localpkg/jquery');
+const json = generator.getMap();
+assert.ok(json.scopes['./'].jquery.includes('@2'));
