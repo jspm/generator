974a75b6a6e9e77ea0768df380e3ac32d8ce173e
Guy Bedford <guybedford@gmail.com>
2022-06-01 15:11:34 -0700

    fixup remaining test

--- test/utils/resolve.test.js
+++ test/utils/resolve.test.js
@@ -3,11 +3,12 @@ import assert from 'assert';
 
 const generator = new Generator({
   mapUrl: import.meta.url,
+  env: ['source']
 });
 
 await generator.install('@jspm/generator');
 
-const generatorUrl = new URL('../../dist/generator.js', import.meta.url).href;
+const generatorUrl = new URL('../../lib/generator.js', import.meta.url).href;
 
 assert.strictEqual(generator.resolve('@jspm/generator'), generatorUrl);
-assert.strictEqual(generator.resolve('#fetch', generatorUrl), new URL('./fetch-native.js', generatorUrl).href);
+assert.strictEqual(generator.resolve('#fetch', generatorUrl), new URL('./common/fetch-native.js', generatorUrl).href);
