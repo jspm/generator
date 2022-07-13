41bd74751c7afb54d26c94ac084a38c5309e5dc0
Guy Bedford <guybedford@gmail.com>
2022-07-12 23:32:51 -0700

    handle npm latest tags

---
+++ test/api/npmdep.test.js
@@ -0,0 +1,12 @@
+import { Generator } from '@jspm/generator';
+import assert from 'assert';
+
+const generator = new Generator({
+  mapUrl: import.meta.url,
+  defaultProvider: 'jspm',
+  env: ['production', 'browser']
+});
+
+await generator.install('@lit-async/ssr-client@1.0.0-rc.1/directives/server-until.js');
+const json = generator.getMap();
+assert.strictEqual(json.imports['@lit-async/ssr-client/directives/server-until.js'], 'https://ga.jspm.io/npm:@lit-async/ssr-client@1.0.0-rc.1/directives/server-until.js');
