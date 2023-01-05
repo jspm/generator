e5ed2e7c79b6ed0c9e785217368fbb12c28fd3d5
Guy Bedford <guybedford@gmail.com>
2023-01-05 16:23:42 +0200

    fix: CI

--- test/utils/lookup.test.js
+++ test/utils/lookup.test.js
@@ -1,13 +1,13 @@
 import { lookup } from '@jspm/generator';
 import assert from 'assert';
 
-const { install, resolved } = await lookup('jquery@3');
+const { install, resolved } = await lookup('jquery@3.5');
 
 assert.strictEqual(install.target.registry, 'npm');
 assert.strictEqual(install.target.name, 'jquery');
-assert.strictEqual(install.target.range, '^3.0');
+assert.strictEqual(install.target.range, '3.5');
 assert.strictEqual(install.subpath, '.');
 assert.strictEqual(install.alias, 'jquery');
 assert.strictEqual(resolved.registry, 'npm');
 assert.strictEqual(resolved.name, 'jquery');
-assert.strictEqual(resolved.version, '3.6.1');
+assert.strictEqual(resolved.version, '3.5.1');
