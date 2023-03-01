ebf8cf93f185cefa37921d45d4d23ecd8086300f
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2023-03-01 15:23:28 +0200

    update nodemodules test

--- test/providers/nodemodules.test.js
+++ test/providers/nodemodules.test.js
@@ -1,7 +1,7 @@
 import { Generator } from "@jspm/generator";
 import assert from "assert";
 
-const generator = new Generator({
+let generator = new Generator({
   mapUrl: new URL("../../", import.meta.url),
   defaultProvider: "nodemodules",
 });
@@ -9,8 +9,34 @@ const generator = new Generator({
 await generator.install("lit-element");
 await generator.install("lit-html");
 
-const json = generator.getMap();
+let json = generator.getMap();
+assert.strictEqual(
+  json.imports["lit-element"],
+  "./node_modules/lit-element/lit-element.js"
+);
+assert.strictEqual(
+  json.scopes["./node_modules/lit-element/"]["lit-html/"],
+  "./node_modules/lit-html/"
+);
 
+// Check that we can reinstall using the jspm.io provider, and then go back to
+// the nodemodules provider, without affecting the resolutions:
+
+generator = new Generator({
+  mapUrl: new URL("../../", import.meta.url),
+  defaultProvider: "jspm.io",
+  inputMap: json,
+});
+await generator.reinstall();
+json = generator.getMap();
+
+generator = new Generator({
+  mapUrl: new URL("../../", import.meta.url),
+  defaultProvider: "nodemodules",
+  inputMap: json,
+});
+await generator.reinstall();
+json = generator.getMap();
 assert.strictEqual(
   json.imports["lit-element"],
   "./node_modules/lit-element/lit-element.js"
