5667d8faa8192c7e81b5317d2bd05e02b1a6fbaa
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2023-03-13 11:32:27 +0200

    wip: add test for top-level install resolutions

--- test/api/resolutions.test.js
+++ test/api/resolutions.test.js
@@ -1,7 +1,22 @@
 import { Generator } from "@jspm/generator";
 import assert from "assert";
 
-const generator = new Generator({
+// Test primary resolutions:
+let generator = new Generator({
+  mapUrl: import.meta.url,
+  env: ["production", "browser"],
+  resolutions: {
+    semver: "6.2.0",
+  },
+});
+
+await generator.install("semver@latest");
+let json = generator.getMap();
+assert.ok(json.imports["semver"]);
+assert.ok(json.imports["semver"].includes("6.2.0"));
+
+// Test secondary resolutions:
+generator = new Generator({
   mapUrl: import.meta.url,
   env: ["production", "browser"],
   resolutions: {
@@ -10,7 +25,7 @@ const generator = new Generator({
 });
 
 await generator.install("@babel/core@7.16.0");
-const json = generator.getMap();
+json = generator.getMap();
 
 assert.ok(json.imports["@babel/core"]);
 assert.ok(Object.keys(json.scopes["https://ga.jspm.io/"]).length > 20);
