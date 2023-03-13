568b89c7794cc3f39b44d2f8618e602866e22280
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2023-03-13 17:15:41 +0200

    chore: add some tests around freeze installs and ensure they permit new locks (#266)

--- test/api/install.test.js
+++ test/api/install.test.js
@@ -6,14 +6,16 @@ const generator = new Generator({
     imports: {
         react: "https://ga.jspm.io/npm:react@17.0.1/dev.index.js",
     },
+    scopes: {
+      "https://ga.jspm.io/": {
+        "lit-html": "https://ga.jspm.io/npm:lit-html@2.6.0/lit-html.js",
+      }
+    },
   },
   mapUrl: import.meta.url,
   env: ["production", "browser"],
   freeze: true, // lock versions
 });
-
-await generator.install();
-const json = generator.getMap();
 
 // Install with too many arguments should throw:
 try {
@@ -23,8 +25,28 @@ try {
   /* expected to throw */
 }
 
-// Install with no arguments should install all pins:
+// Install with no arguments should install all top-level pins.
+await generator.install();
+let json = generator.getMap();
+
 assert.strictEqual(
   json.imports.react,
   "https://ga.jspm.io/npm:react@17.0.1/index.js"
+);
+
+// Installing a new dependency with freeze should not throw, but it should
+// never bump versions from the import map:
+await generator.install(["lit@2.6.1", "lit-html"]);
+json = generator.getMap();
+
+assert.strictEqual(
+  json.imports.lit,
+  "https://ga.jspm.io/npm:lit@2.6.1/index.js",
+);
+
+// Even though latest for lit-html is 2.6.1, it should remain locked due to
+// the freeze option being set:
+assert.strictEqual(
+  json.imports["lit-html"],
+  "https://ga.jspm.io/npm:lit-html@2.6.0/lit-html.js",
 );
