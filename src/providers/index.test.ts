1f84ea98aa98f8c0ce4dc59e1c7ac5d2f15abe8e
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2023-03-15 18:10:19 +0200

    fix: alias jspm to jspm.io for backwards compatibility (#275)

--- src/providers/index.test.ts
+++ src/providers/index.test.ts
@@ -2,7 +2,6 @@ import { getDefaultProviderStrings } from "@jspm/generator/providers/index.js";
 import assert from "assert";
 
 let ps: string[] = getDefaultProviderStrings();
-console.log(ps);
 assert.ok(ps.includes("jspm.io"));
 assert.ok(ps.includes("jspm.io#system"));
 assert.ok(ps.includes("esm.sh"));
