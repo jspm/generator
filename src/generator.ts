220ce6e3cbfd90bd175316ec3a70957d52b6bf0d
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2023-04-04 06:42:10 +0200

    fix: install with no args should not reinstall custom mappings (#287)

--- src/generator.ts
+++ src/generator.ts
@@ -1002,10 +1002,12 @@ export class Generator {
         "Install takes no arguments, a single install target, or a list of install targets."
       );
 
-    // If there are no arguments, install all top-level pins.
+    // If there are no arguments, then we reinstall all the top-level locks:
     if (!install) {
       await this.traceMap.processInputMap;
-      return this.install(this.traceMap.pins);
+      return this.install(
+        Object.keys(this.traceMap.installer.installs.primary)
+      );
     }
 
     // Split the case of multiple install targets:
