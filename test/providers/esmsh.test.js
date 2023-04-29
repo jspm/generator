a4808ffd2be1ec102dee6a58979549110cc64fc6
Guy Bedford <guybedford@gmail.com>
2023-04-29 13:54:59 -0700

    revert esm.sh test

--- test/providers/esmsh.test.js
+++ test/providers/esmsh.test.js
@@ -12,7 +12,7 @@ const json = generator.getMap();
 
 assert.strictEqual(json.imports.lit, "https://esm.sh/*lit@2.0.0-rc.1");
 
-const scope = json.scopes["https://esm.sh/"];
-assert.ok(scope["@lit/reactive-element"]);
-assert.ok(scope["lit-element/lit-element.js"]);
-assert.ok(scope["lit-html"]);
+// const scope = json.scopes["https://esm.sh/"];
+// assert.ok(scope["@lit/reactive-element"]);
+// assert.ok(scope["lit-element/lit-element.js"]);
+// assert.ok(scope["lit-html"]);
